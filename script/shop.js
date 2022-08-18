224389ebb43243513e3805ba407664f5fbd40668
B-usman <babatundeusman82@gmail.com>
2022-08-18 07:46:49 +0100

    shop page edited, Javascript added

---
+++ script/shop.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById('buy').onclick = function(){
+    document.getElementById('now').style.display = "block"
+    document.getElementById('main').style.display = "none"
+    document.getElementById('body').style.backgroundColor = "darkgrey"
+}
